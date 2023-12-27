224e423ed8679f35e7e2fb7c6af3325d7ba99a74
LeeJiWon <dlwldnjs1009@gmail.com>
2023-12-27 17:16:37 +0900

    feat: 개발환경 세팅

---
+++ f-log/src/main/java/com/f_log/flog/FLogApplication.java
@@ -0,0 +1,13 @@
+package com.f_log.flog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FLogApplication.class, args);
+    }
+
+}
---
+++ f-log/src/test/java/com/f_log/flog/FLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f_log.flog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
