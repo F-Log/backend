346dcdc9cd9f362095bbdd6c6fb6b527d3827a60
Yoo Hyunseok <brianyhs@naver.com>
2023-12-28 17:38:34 +0900

    feat: add Diet entity

---
+++ src/main/java/com/f_log/flog/domain/Diet.java
@@ -0,0 +1,44 @@
+package com.f_log.flog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Diet {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "diet_id")
+    private Long id;
+
+    /* TODO: s3 url 추가 */
+
+    @OneToMany(mappedBy = "diet")
+    private List<Food> foods = new ArrayList<>();
+
+    @Column(name = "total_carbohydrate")
+    private int totalCarbohydrate;
+
+    @Column(name = "total_protein")
+    private int totalProtein;
+
+    @Column(name = "total_fat")
+    private int totalFat;
+
+    @Column(name = "total_sodium")
+    private int totalSodium;
+
+    @Column(name = "total_cholesterol")
+    private int totalCholesterol;
+
+    @Column(name = "total_carbohydrate")
+    private LocalDateTime mealDate;
+
+    
+}
