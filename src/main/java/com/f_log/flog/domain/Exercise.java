07f183723aa34230a53f248be7335fc245cf1695
Yoo Hyunseok <brianyhs@naver.com>
2023-12-29 16:08:35 +0900

    feat: add Exercise entity

---
+++ src/main/java/com/f_log/flog/domain/Exercise.java
@@ -0,0 +1,30 @@
+package com.f_log.flog.domain;
+
+import com.f_log.flog.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Exercise extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "exercise_id")
+    private Long id;
+
+    @Column(name = "exercise_type")
+    private String exerciseType;
+
+    @Column(name = "target_weight")
+    private float targetWeight;
+
+    @Column(name = "exercise_frequency")
+    private float exerciseFrequency;
+
+    @Enumerated(EnumType.STRING)
+    private ExerciseIntensity exerciseIntensity;
+
+    @Enumerated(EnumType.STRING)
+    private ExercisePurpose exercisePurpose;
+}
---
+++ src/main/java/com/f_log/flog/domain/ExercisePurpose.java
@@ -0,0 +1,5 @@
+package com.f_log.flog.domain;
+
+public enum ExercisePurpose {
+    LOSE_WEIGHT, MAINTAIN_WEIGHT, GAIN_WEIGHT, GAIN_MUSCLE
+}
