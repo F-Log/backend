4b22af2210b4cb5d689282c8a5a66a5ef93b6547
LeeJiWon <dlwldnjs1009@gmail.com>
2024-03-07 01:12:02 +0900

    feat: 공공데이터 api batch insert 구현

--- src/main/java/com/f_log/flog/food/service/FoodService.java
+++ src/main/java/com/f_log/flog/food/service/FoodService.java
@@ -3,17 +3,25 @@ package com.f_log.flog.food.service;
 import com.f_log.flog.food.domain.Food;
 import com.f_log.flog.food.dto.FoodRequestDto;
 import com.f_log.flog.food.repository.FoodRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.util.StopWatch;
 
 @Service
 @RequiredArgsConstructor
 public class FoodService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     private final FoodRepository foodRepository;
 
@@ -61,18 +69,47 @@ public class FoodService {
         }
     }
 
+//    @Transactional
+//    public void saveAllFoods(List<Food> foods) {
+//
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//
+//        List<Food> filteredFoods = new ArrayList<>();
+//        for (Food food : foods) {
+//            List<Food> existingFood = foodRepository.findByFoodNameAndIsDeletedFalse(food.getFoodName());
+//            if (existingFood.isEmpty()) {
+//                filteredFoods.add(food);
+//            }
+//        }
+//        if (!filteredFoods.isEmpty()) {
+//            foodRepository.saveAll(filteredFoods);
+//        }
+//
+//        stopWatch.stop();
+//
+//        System.out.println("Time taken to save foods: " + stopWatch.getTotalTimeMillis() + "ms");
+//    }
+
     @Transactional
-    public void saveAllFoods(List<Food> foods) {
-        List<Food> filteredFoods = new ArrayList<>();
-        for (Food food : foods) {
-            List<Food> existingFood = foodRepository.findByFoodNameAndIsDeletedFalse(food.getFoodName());
-            if (existingFood.isEmpty()) {
-                filteredFoods.add(food);
+    public void saveFoodInBatches(List<Food> foods, int batchSize) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        for (int i = 0; i < foods.size(); i++) {
+            if (foods.get(i).getId() == null) {
+                entityManager.persist(foods.get(i));
+            } else {
+                entityManager.merge(foods.get(i));
+            }
+            if ((i + 1) % batchSize == 0) {
+                entityManager.flush();
+                entityManager.clear();
             }
         }
-        if (!filteredFoods.isEmpty()) {
-            foodRepository.saveAll(filteredFoods);
-        }
+        entityManager.flush();
+        stopWatch.stop();
+        System.out.println("Batch save time for Foods: " + stopWatch.getTotalTimeMillis() + "ms");
     }
 
     public List<Food> findAllFoods(){
