4ccec669273bf2c88948b7340b45c4af43c7c959
LeeJiWon <dlwldnjs1009@gmail.com>
2024-03-09 17:05:51 +0900

    chore: dietfeedback ID 생성전략 UUID로 변경

--- src/main/java/com/f_log/flog/dietfeedback/repository/DietFeedbackRepository.java
+++ src/main/java/com/f_log/flog/dietfeedback/repository/DietFeedbackRepository.java
@@ -2,13 +2,14 @@ package com.f_log.flog.dietfeedback.repository;
 
 import com.f_log.flog.diet.domain.Diet;
 import com.f_log.flog.dietfeedback.domain.DietFeedback;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface DietFeedbackRepository extends JpaRepository<DietFeedback, Long> {
+public interface DietFeedbackRepository extends JpaRepository<DietFeedback, UUID> {
 
-    Optional<DietFeedback> findByIdAndIsDeletedFalse(Long id);
+    Optional<DietFeedback> findByDietfeedbackUuidAndIsDeletedFalse(UUID dietfeedbackUuid);
 }
